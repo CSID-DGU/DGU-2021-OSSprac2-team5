1b899a41640a46782063836aa70223a1b784f6d3
hyun7520 <sanghyun123452@gmail.com>
2021-12-04 11:49:13 +0900

    update

--- app/server.py
+++ app/server.py
@@ -3,19 +3,35 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def student():
-   return render_template('student_info.html')
+def main():
+   return render_template('main.html')
 
-@app.route('/result', methods = ['POST', 'GET'])
-def result():
+@app.route('/detail', methods = ['POST', 'GET'])
+def detail():
    if request.method == 'POST':
       result = dict()
       result['Name'] = request.form.get('Name')
-      result['StudentNumber'] = request.form.get('StudentNumber')
-      result['Gender'] = request.form.get('Gender')
       result['Major'] = request.form.get('Major')
-      result['languages'] = ', '.join(request.form.getlist('languages'))
-      return render_template("result.html",result = result)
+      result['Year'] = request.form.get('Year')
+      result['Desire_metod'] = request.form.get('Desire_metod')
+      result['Vaccine'] = request.form.get('Vaccine')
+      result['Reason'] = request.form.get('Reason')
+      return render_template("detail.html",result = result)
+
+@app.route('/submit', methods = ['POST', 'GET'])
+def submit():
+   uname = request.form.get('uname')
+   umajor = request.form.get('umajor')
+   ugrade = request.form.get('ugrade')
+   if request.method == 'POST':
+      result = dict()
+      result['Name'] = request.form.get('Name')
+      result['Major'] = request.form.get('Major')
+      result['Year'] = request.form.get('Year')
+      result['Desire_metod'] = request.form.get('Desire_metod')
+      result['Vaccine'] = request.form.get('Vaccine')
+      result['Reason'] = request.form.get('Reason')
+      return render_template("submit.html",result = result, uname=uname, uyear=uyear, umajor=umajor)
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", debug=True, port=80)
