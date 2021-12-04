60bbe3aae8b316da007cd73311c12a05f4e64311
hyun7520 <sanghyun123452@gmail.com>
2021-12-04 16:45:57 +0900

    update 3

--- app/server.py
+++ app/server.py
@@ -22,12 +22,13 @@ def apllication():
 def detail():
    if request.method == 'POST':
       result = dict()
-      result['Name'] = request.form.get('Name')
-      result['Major'] = request.form.get('Major')
-      result['Grade'] = request.form.get('Grade')
-      result['Desire_method'] = request.form.get('Desire_method')
-      result['Vaccine'] = request.form.get('Vaccine')
-      result['Reason'] = request.form.get('Reason')
+      result['이름'] = request.form.get('uname')
+      result['전공'] = request.form.get('umajor')
+      result['학년'] = request.form.get('grade')
+      result['희망 수업 방식'] = request.form.get('class')
+      result['백신 접종 여부'] = request.form.get('completion')
+      result['접종 백신 종류'] = request.form.get('vaccine')
+      result['해당 수업방식 선호 이유'] = request.form.get('memo')
       return render_template("detail.html",result = result)
 
 @app.route('/submit', methods = ['POST', 'GET'])
@@ -37,12 +38,13 @@ def submit():
    ugrade = request.form.get('ugrade')
    if request.method == 'POST':
       result = dict()
-      result['Name'] = request.form.get('Name')
-      result['Major'] = request.form.get('Major')
-      result['Grade'] = request.form.get('Grade')
-      result['Desire_method'] = request.form.get('Desire_method')
-      result['Vaccine'] = request.form.get('Vaccine')
-      result['Reason'] = request.form.get('Reason')
+      result['이름'] = request.form.get('uname')
+      result['전공'] = request.form.get('umajor')
+      result['학년'] = request.form.get('grade')
+      result['희망 수업 방식'] = request.form.get('class')
+      result['백신 접종 여부'] = request.form.get('completion')
+      result['접종 백신 종류'] = request.form.get('vaccine')
+      result['해당 수업방식 선호 이유'] = request.form.get('memo')
       return render_template("submit.html",result = result, uname=uname, Grade=Grade, umajor=umajor)
 
 if __name__ == '__main__':
